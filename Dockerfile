8cce09708d9c5447c3f8a2f1b9926de3ed1e2759
Rémi Demol <rde@sirdata.fr>
2020-01-12 23:24:37 +0100

    push Dockerfile & qovery file

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM golang:stretch
+
+RUN go get && go build
+
+
+RUN mkdir -p /app
+COPY qovery-go-client-example-postgresql /app/server
+WORKDIR /app
+
+# Choose the port to publicly expose to the internet
+EXPOSE 8080
+
+# Create a dedicated user to run your app with (for security reasons)
+RUN useradd -ms /bin/bash qovery
+USER qovery
+
+CMD app/server -bind=":8080"
--- postgresql.go
+++ postgresql.go
@@ -6,31 +6,41 @@ import (
 	"github.com/Qovery/qovery-go-client"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
-	"log"
+	"github.com/kataras/iris"
 )
 
 var (
 	configurationFilename = flag.String("config-filename", "../../test_files/local_configuration.json", "Qovery configuration filename")
 	databaseName          = flag.String("dbname", "my-pql", "")
+	bind                  = flag.String("bind", ":8080", "Http port")
 )
 
 func main() {
 	flag.Parse()
+
+	app := iris.New()
+	app.Get("/database", func(ctx iris.Context) {
+		printDbStatus(ctx)
+	})
+	app.Run(iris.Addr(*bind))
+}
+
+func printDbStatus(ctx iris.Context) {
 	qv, err := qovery.New(configurationFilename)
 	if err != nil {
-		log.Fatalf("fail to init qv client: %s", err.Error())
+		ctx.Writef("fail to init qv client: %s", err.Error())
 	}
 
 	dbConf := qv.GetDatabaseConfigurationByName(*databaseName)
 	if dbConf == nil {
-		log.Fatalf("fail to get database name %s", *databaseName)
+		ctx.Writef("fail to get database name %s", *databaseName)
 	}
 
 	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s port=%d", dbConf.Host, dbConf.Username, dbConf.Name, dbConf.Password, dbConf.Port)
 	db, err := gorm.Open("postgres", dbURI)
 	if err != nil {
-		log.Fatalf("fail to connect to dbConf: %s", err.Error())
+		ctx.Writef("fail to connect to dbConf: %s", err.Error())
 	}
 	defer db.Close()
-	log.Printf("connection to '%s' successful", dbConf.Name)
+	ctx.Writef("connection to '%s' successful", dbConf.Name)
 }
